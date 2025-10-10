4a2a68038ad9ca1373b17ead25f82fa31d84e2e5
Chanreach <ctry1@paragoniu.edu.kh>
2025-10-10 16:55:30 +0700

    delete: remove unnecessay files

--- src/routes/join.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import joinController from "../controllers/join.controller.js";
-
-const router = express.Router();
-
-router.post("/boss-fight", joinController.joinBossFight);
-
-export default router;
