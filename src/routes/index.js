c555ab859d8781bdd8fb29736a61bb6a27fa86df
Chanreach <ctry1@paragoniu.edu.kh>
2025-10-23 17:36:40 +0700

    remove: badge route (not used)

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,6 @@ import categoryRoutes from "./category.routes.js";
 import questionRoutes from "./question.routes.js";
 import bossRoutes from "./boss.routes.js";
 import eventBossRoutes from "./event-boss.routes.js";
-import badgeRoutes from "./badge.routes.js";
 import userBadgeRoutes from "./user-badge.routes.js";
 import leaderboardRoutes from "./leaderboard.routes.js";
 
@@ -19,7 +18,6 @@ router.use("/categories", categoryRoutes);
 router.use("/questions", questionRoutes);
 router.use("/bosses", bossRoutes);
 router.use("/event-bosses", eventBossRoutes);
-router.use("/badges", badgeRoutes);
 router.use("/user-badges", userBadgeRoutes);
 router.use("/leaderboards", leaderboardRoutes);
 
